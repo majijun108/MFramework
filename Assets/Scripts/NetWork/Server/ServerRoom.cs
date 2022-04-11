e14f212b0dfd2290d30f468c0e7490addbb17014
seeker <majijun108@sina.com>
2022-04-11 19:30:24 +0800

    帧同步逻辑修改

--- Assets/Scripts/NetWork/Server/ServerRoom.cs
+++ Assets/Scripts/NetWork/Server/ServerRoom.cs
@@ -16,11 +16,18 @@ namespace Server
         public object obj;
     }
     //服务器房间 跑在另外的线程
-    public class ServerRoom:IMessageDispatcher
+    public class ServerRoom : IMessageDispatcher
     {
+        enum RoomState
+        {
+            WaitingForPlayer,
+            WaitingForBattle,
+            InBattle,
+            Disposed
+        }
+
         private const int FrameRate = 30;
 
-        public bool IsDisposed { get; private set; } = false;
         private UDPNetProxy m_Server;
         private RoomInfo m_roomInfo;
         private int m_broadcastPort;//广播的端口号
@@ -28,9 +35,11 @@ namespace Server
         private int m_mainPlayerPort;//主机的端口
 
         private int m_frameDelta;//每帧的时间
-        private DateTime m_startTime;
+
+        private List<PlayerInfo> m_readyPlayer = new List<PlayerInfo>();//已经准备好的客户端
 
         private Queue<ServerCmdInfo> m_cmdQueue = new Queue<ServerCmdInfo>();
+        private RoomState m_roomState;
 
         static int roomID = 1;
 
@@ -53,9 +62,10 @@ namespace Server
                 RoomID = roomID++
             };
             m_frameDelta = 1000 / FrameRate;
-            m_startTime = DateTime.Now;
             serverIP = NetHelper.GetLocalIP();
             serverPort = port;
+            m_readyPlayer.Clear();
+            m_roomState = RoomState.WaitingForPlayer;
         }
 
         public void Start(PlayerInfo mainPlayer) 
@@ -72,14 +82,27 @@ namespace Server
             Run();
         }
 
-        //向服务器push指令 会在异步调用
-        public void PushCommand(ServerCmdInfo info) 
+        async void Run()
         {
-            lock (m_cmdQueue) 
+            while (true)
             {
-                m_cmdQueue.Enqueue(info);
+                if (m_roomState == RoomState.Disposed)
+                    return;
+                Tick();
+                await Task.Delay(m_frameDelta);
             }
         }
+
+        void Tick()
+        {
+            if (m_Server == null || m_Server.IsDisposed)
+                return;
+            ExcuteCmd();
+            BattleTick();
+
+            m_Server.Update();
+        }
+
 
         //创建完房间 广播服务器
         private void BroadcastRoomInfo()
@@ -90,14 +113,28 @@ namespace Server
                     m_broadcastPort);
         }
 
-        async void Run() 
+
+        //向服务器push指令 会在异步调用
+        public void PushCommand(ServerCmdInfo info)
         {
-            while (true) 
+            lock (m_cmdQueue)
             {
-                if (IsDisposed)
-                    return;
-                Tick();
-                await Task.Delay(FrameRate);
+                m_cmdQueue.Enqueue(info);
+            }
+        }
+
+        void CmdHandler(ServerCmdInfo cmd) 
+        {
+            switch (cmd.type)
+            {
+                case SERVER_CMD_TYPE.SEND_ROOMINFO:
+                    PlayerInfo player = cmd.obj as PlayerInfo;
+                    if (player != null && m_roomInfo != null)
+                    {
+                        m_Server.Send((byte)MsgType.S2C_RoomInfo, m_roomInfo
+                            , NetHelper.GetIPEndPoint(player.ClientIP, player.ClientPort));
+                    }
+                    break;
             }
         }
 
@@ -111,28 +148,11 @@ namespace Server
                 while (m_cmdQueue.Count > 0)
                 {
                     var cmd = m_cmdQueue.Dequeue();
-                    switch (cmd.type) 
-                    {
-                        case SERVER_CMD_TYPE.SEND_ROOMINFO:
-                            PlayerInfo player = cmd.obj as PlayerInfo;
-                            if (player != null && m_roomInfo != null)
-                            {
-                                m_Server.Send((byte)MsgType.S2C_RoomInfo, m_roomInfo
-                                    , NetHelper.GetIPEndPoint(player.ClientIP, player.ClientPort));
-                            }
-                            continue;
-                    }
+                    CmdHandler(cmd);
                 }
             }
         }
 
-        void Tick() 
-        {
-            if (m_Server == null || m_Server.IsDisposed)
-                return;
-            ExcuteCmd();
-            m_Server.Update();
-        }
 
         int GetPlayerIndex(PlayerInfo player)
         {
@@ -145,6 +165,19 @@ namespace Server
                     return i;
             }
             return -1;
+        }
+
+        bool IsInRoom(PlayerInfo player,out int index) 
+        {
+            index = -1;
+            if (m_roomInfo == null)
+                return false;
+            if (player == null)
+                return false;
+            index = GetPlayerIndex(player);
+            if (index < 0)
+                return false;
+            return true;
         }
 
         //关闭房间
@@ -177,13 +210,40 @@ namespace Server
 
         public void Dispose()
         {
-            if (IsDisposed)
+            if (m_roomState == RoomState.Disposed)
                 return;
-            IsDisposed = true;
+            m_roomState = RoomState.Disposed;
             m_Server.Dispose();
             m_Server = null;
             m_roomInfo = null;
+            m_readyPlayer.Clear();
+            m_readyPlayer = null;
         }
+
+        #region 开始战斗
+
+        private float m_startTime;
+        private int m_Tick;
+        private int m_tickSinceGameStart =>
+            (int)(LTime.realtimeSinceStartup - m_startTime) / m_frameDelta;
+
+        void StartBattle() 
+        {
+            m_roomState = RoomState.InBattle;
+            m_startTime = LTime.realtimeSinceStartup;
+            m_Tick = 0;
+        }
+        //战斗跑帧
+        void BattleTick() 
+        {
+            if (m_roomState != RoomState.InBattle)
+                return;
+            while (m_Tick < m_tickSinceGameStart) 
+            {
+
+            }
+        }
+        #endregion
 
 
 
@@ -192,22 +252,22 @@ namespace Server
         {
             if (player == null || m_roomInfo == null)
                 return;
-            if (m_roomInfo.Players.Count >= m_roomInfo.MaxCount)
+            if (m_roomState != RoomState.WaitingForPlayer)
                 return;
             int index = GetPlayerIndex(player);
             if (index >= 0)
                 return;
             m_roomInfo.Players.Add(player);
             Broadcast(MsgType.S2C_UpdateRoomInfo, m_roomInfo);
+
+            if (m_roomInfo.Players.Count == m_roomInfo.MaxCount)
+                m_roomState = RoomState.WaitingForBattle;
         }
 
         //客户端请求退出房间
         void OnC2S_ReqExitRoom(PlayerInfo player)
         {
-            if (player == null || m_roomInfo == null)
-                return;
-            int index = GetPlayerIndex(player);
-            if (index == -1)
+            if (!IsInRoom(player,out int index))
                 return;
 
             if (IsMainPlayer(player))
@@ -219,19 +279,39 @@ namespace Server
             m_Server.Send((byte)MsgType.S2C_ExitRoom, m_roomInfo,
                 NetHelper.GetIPEndPoint(player.ClientIP, player.ClientPort));
             Broadcast(MsgType.S2C_UpdateRoomInfo, m_roomInfo);
+
+            if (m_roomInfo.Players.Count < m_roomInfo.MaxCount)
+                m_roomState = RoomState.WaitingForPlayer;
         }
 
         //请求开始游戏
         void On_C2S_ReqStartGame(PlayerInfo player) 
         {
-            if (m_roomInfo == null)
+            if (!IsInRoom(player, out int index))
                 return;
-            int index = GetPlayerIndex(player);
-            if (index == -1)
-                return;
-            if (m_roomInfo.Players.Count < m_roomInfo.MaxCount)
+            if (m_roomState != RoomState.WaitingForBattle)
                 return;
             Broadcast(MsgType.S2C_StartGame, m_roomInfo);
+        }
+
+        //客户端准备完成
+        void On_C2S_ClientReady(PlayerInfo player) 
+        {
+            if (!IsInRoom(player, out int index))
+                return;
+            for (int i = 0; i < m_readyPlayer.Count; i++)
+            {
+                var p = m_readyPlayer[i];
+                if (p.ClientIP == player.ClientIP && p.ClientPort == player.ClientPort)
+                    return;
+            }
+
+            m_readyPlayer.Add(player);
+            if (m_readyPlayer.Count == m_roomInfo.MaxCount) 
+            {
+                m_readyPlayer.Clear();
+                StartBattle();
+            }
         }
 
         public void Dispatch(Session session, byte opcode, object message)
@@ -247,6 +327,9 @@ namespace Server
                     break;
                 case MsgType.C2S_ReqStartGame://请求开始游戏
                     On_C2S_ReqStartGame(message as PlayerInfo);
+                    break;
+                case MsgType.C2S_ClientReady://客户端发送准备完毕
+                    On_C2S_ClientReady(message as PlayerInfo);
                     break;
             }
         }
