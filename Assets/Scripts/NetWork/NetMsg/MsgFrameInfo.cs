f95544887cd226f1cbc3fe8ebfd29a98d704abcd
seeker <majijun108@sina.com>
2022-04-14 21:45:14 +0800

    加入帧同步数据

---
+++ Assets/Scripts/NetWork/NetMsg/MsgFrameInfo.cs
@@ -0,0 +1,37 @@
+﻿using Lockstep.Math;
+using System;
+using System.Collections.Generic;
+
+public class Msg_FrameInfo : BaseFormater
+{
+    public int Tick;
+    public Msg_PlayerInput[] Inputs;
+
+    public override void Deserialize(Deserializer reader)
+    {
+        
+    }
+
+    public override void Serialize(Serializer writer)
+    {
+        
+    }
+}
+
+public class Msg_PlayerInput : BaseFormater
+{
+    public int Tick;
+    public int PlayerID;
+    public int SkillID;
+    public LFloat MoveAngle;//移动的角度
+
+    public override void Deserialize(Deserializer reader)
+    {
+        
+    }
+
+    public override void Serialize(Serializer writer)
+    {
+        
+    }
+}
