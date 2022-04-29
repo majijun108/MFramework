65562825c70a9aff4d3b8f24df3c505acc654812
seeker <majijun108@sina.com>
2022-04-29 18:22:10 +0800

    GJK碰撞

--- Assets/Test/GJKCollider.cs
+++ Assets/Test/GJKCollider.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace GJKTest
 {
+    //检测是否碰撞
     public class GJKCollider
     {
 
@@ -47,20 +48,104 @@ namespace GJKTest
             Vector2 fastB = shapeB.FindFastestPointInDir(-dir);
             return fastA - fastB;
         }
+
+        class Simplex
+        {
+            private List<Vector2> points = new List<Vector2>();
+
+
+            public void Clear()
+            {
+                points.Clear();
+            }
+
+            public void Add(Vector2 point)
+            {
+                points.Add(point);
+            }
+
+            public bool Contains(Vector2 point)
+            {
+                return GJKUtil.Cotains(points, point);
+            }
+
+            public Vector2 FindNextDir()
+            {
+                if (points.Count == 2)
+                {
+                    Vector2 point = GJKUtil.GetOriginFootPoint(points[0], points[1]);
+                    return Vector2.zero - point;
+                }
+                else if (points.Count == 3)
+                {
+                    Vector2 point1 = GJKUtil.GetOriginFootPoint(points[2], points[0]);
+                    Vector2 point2 = GJKUtil.GetOriginFootPoint(points[2], points[1]);
+                    if (point1.sqrMagnitude < point2.sqrMagnitude)
+                    {
+                        points.RemoveAt(1);
+                        return Vector2.zero - point1;
+                    }
+                    else
+                    {
+                        points.RemoveAt(0);
+                        return Vector2.zero - point2;
+                    }
+                }
+                else
+                    return Vector2.zero;
+            }
+        }
+    }
+
+    //求多边形之间的距离
+    public class GJKDistancce 
+    {
+        private Simplex m_simplex = new Simplex();
+
+        public bool CheckCollider(Shape a, Shape b) 
+        {
+            m_simplex.Clear();
+            
+            Vector2 dir = findFirstDir(a,b);
+            SupportPoint support = new SupportPoint();
+            GJKUtil.Support(a, b, dir, ref support);
+            m_simplex.Add(support);
+            GJKUtil.Support(a,b,-dir,ref support);
+            m_simplex.Add(support);
+
+            dir = -GJKUtil.FindClosestToOrigin(m_simplex[0].Point,m_simplex[1].Point);
+        }
+
+        Vector2 findFirstDir(Shape a, Shape b)
+        {
+            Vector2 dir = a[0] - b[0];
+            if (dir.sqrMagnitude < GJKUtil.epsilon)
+            {
+                dir = a[1] - b[0];
+            }
+            return dir;
+        }
     }
 
     //单纯形
     public class Simplex 
     {
-        private List<Vector2> points = new List<Vector2>();
+        private List<SupportPoint> points = new List<SupportPoint>();
 
+        public SupportPoint this[int index] 
+        {
+            get 
+            {
+                return points[index];
+            }
+        }
 
         public void Clear() 
         {
             points.Clear();
         }
 
-        public void Add(Vector2 point) 
+        public void Add(SupportPoint point) 
         {
             points.Add(point);
         }
@@ -74,13 +159,13 @@ namespace GJKTest
         {
             if (points.Count == 2)
             {
-                Vector2 point = GJKUtil.GetOriginFootPoint(points[0], points[1]);
+                Vector2 point = GJKUtil.GetOriginFootPoint(points[0].Point, points[1].Point);
                 return Vector2.zero - point;
             }
             else if (points.Count == 3) 
             {
-                Vector2 point1 = GJKUtil.GetOriginFootPoint(points[2], points[0]);
-                Vector2 point2 = GJKUtil.GetOriginFootPoint(points[2], points[1]);
+                Vector2 point1 = GJKUtil.GetOriginFootPoint(points[2].Point, points[0].Point);
+                Vector2 point2 = GJKUtil.GetOriginFootPoint(points[2].Point, points[1].Point);
                 if (point1.sqrMagnitude < point2.sqrMagnitude)
                 {
                     points.RemoveAt(1);
--- Assets/Test/GJKUtil.cs
+++ Assets/Test/GJKUtil.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 namespace GJKTest
 {
+    public struct SupportPoint 
+    {
+        public Vector2 Point;
+        public Vector2 FromeA;
+        public Vector2 FromeB;
+    }
+
     public static class GJKUtil
     {
-
+        public static float epsilon = 0.00001f;
         static int Cross(Vector2 a, Vector2 b, Vector2 c) 
         {
             Vector2 ab = b - a;
@@ -17,6 +24,7 @@ namespace GJKTest
             return -1;
         }
 
+        //多边形是否包含一个点
         public static bool Cotains(List<Vector2> shape, Vector2 point) 
         {
             if(shape.Count < 3)
@@ -36,6 +44,25 @@ namespace GJKTest
             return true;
         }
 
+        public static bool Cotains(List<SupportPoint> shape, Vector2 point)
+        {
+            if (shape.Count < 3)
+                return false;
+            float slider = Cross(shape[0].Point, shape[1].Point, shape[2].Point);
+            int n = shape.Count;
+            for (int i = 0; i < n; i++)
+            {
+                int nextIndex = (i + 1) % n;
+                float cross = Cross(shape[i].Point, shape[nextIndex].Point, point);
+                if (cross == 0)
+                    return true;
+                if (cross != slider)
+                    return false;
+            }
+
+            return true;
+        }
+
         //求原点到直线ab的垂足
         public static Vector2 GetOriginFootPoint(Vector2 a, Vector2 b) 
         {
@@ -44,6 +71,36 @@ namespace GJKTest
             float dot = Vector2.Dot(ab, ao) / Vector2.SqrMagnitude(ab);
             var res = a + ab * dot;
             return res;
+        }
+
+        //求闵可夫斯基差集的最远点
+        public static void Support(Shape a, Shape b, Vector2 dir,ref SupportPoint point) 
+        {
+            Vector2 fastestA = a.FindFastestPointInDir(dir);
+            Vector2 fastestB = b.FindFastestPointInDir(dir);
+            point.Point = fastestA - fastestB;
+            point.FromeA = fastestA;
+            point.FromeB = fastestB;
+        }
+
+        //寻找线段上距离原点最近的点
+        public static Vector2 FindClosestToOrigin(Vector2 a, Vector2 b) 
+        {
+            Vector2 ab = b - a;
+            float sqrDis = ab.sqrMagnitude;
+            if (sqrDis < epsilon)
+                return a;
+
+            Vector2 ao = Vector2.zero - a;
+            var dot = Vector2.Dot(ab, ao);
+            if (dot < 0) 
+            {
+                return a;
+            }
+            float disNormal = dot / sqrDis;
+            if (disNormal > 1)
+                return b;
+            return a + disNormal * ab;
         }
     }
 }
